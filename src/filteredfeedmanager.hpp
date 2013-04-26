8c4bfc6b2f737cc216117faa3271ffce6a9b2e52
Marc Plano-Lesay <marc.planolesay@gmail.com>
2013-04-26 07:57:46 +0200

    Style

--- src/filteredfeedmanager.hpp
+++ src/filteredfeedmanager.hpp
@@ -23,9 +23,9 @@ namespace ttrss {
 		Q_INVOKABLE void resetCategoryFilter();
 
 	private Q_SLOTS:
-	void handleAdd(const QVariantList& sourceIndexPath);
-	void handleUpdate(const QVariantList& sourceIndexPath);
-	void handleRemove(const QVariantList& sourceIndexPath);
+		void handleAdd(const QVariantList& sourceIndexPath);
+		void handleUpdate(const QVariantList& sourceIndexPath);
+		void handleRemove(const QVariantList& sourceIndexPath);
 
 	private:
 	    bool isSourceIndexFiltered(const QVariantList& sourceIndexPath) const;
